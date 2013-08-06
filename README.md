f8c5aaaa0660e9933b6b1883593f5f153e4ecf9c
Jean-David Gadina <macmade@xs-labs.com>
2013-08-06 10:55:11 +0200

    32 bits SSE2 optimised version of memset() is now available.

--- README.md
+++ README.md
@@ -34,8 +34,8 @@ Test consists of **10000000** (10 millions) calls to `memset()` with a 4096 byes
     OS X memset() / 64bits:				0.906685 seconds
     XEOS memset() / 64bits:				0.778556 seconds	<-- Best result
     
-    OS X memset() / 32bits: 		  	0.869590
-    XEOS memset() / 32bits:    			N/A (not developed yet)
+    OS X memset() / 32bits: 		  	0.869590 seconds
+    XEOS memset() / 32bits:    			0.798635 seconds    <-- Best result
     
 License
 -------
--- source/memset.32.s
+++ source/memset.32.s
@@ -157,9 +157,254 @@ _xeos_memset:
 ;-------------------------------------------------------------------------------
 _memset32_sse2:
     
-    mov eax,    [ esp + 4 ]
+    ; Creates a stack frame, so we can save registers, making them available
+    ; to use. Otherwise, only 3 registers are safe, which is not enough here
+    push    ebp
+    mov     ebp, esp
     
-    ret
+    ; Saves EDI, ESI and EBX as we are going to use them
+    push    edi
+    push    esi
+    push    ebx
+    
+    ; Gets the arguments from the stack
+    mov     edi,        [ ebp +  8 ]
+    mov     esi,        [ ebp + 12 ]
+    mov     edx,        [ ebp + 16 ]
+    
+    ; Saves the original memory pointer, as we'll need to return it
+    mov     ebx,        edi
+    
+    ; memset() is often called to set a buffer to zero, so prepare xmm0
+    ; for such a case
+    pxor    xmm0,       xmm0
+    
+    ; Checks if bytes needs to be set to zero
+    cmp     esi,        0
+    je      .padded
+    
+    .pad:
+        
+        ; A character other than zero will be used. Fills the ESI registers
+        ; with four time the character value, so it will be easier to fill
+        ; a XMM register with the character to set
+        mov     ecx,        esi
+        shl     esi,        8
+        or      esi,        ecx
+        mov     ecx,        esi
+        shl     esi,        16
+        or      esi,        ecx
+        
+        ; Fills the XMM0 register with the character to write, so we'll be
+        ; able to write 16 bytes at a time
+        movd    xmm0,       esi
+        shufps  xmm0,       xmm0,   0x00
+        
+    .padded:
+        
+        ; Aligns the memory pointer in EDI to a 16-byte boundary,
+        ; so we can safelfy use the SSE instructions
+        and     edi,        -16
+        
+        ; Gets the number of misaligned bytes in the original memory pointer (EBX)
+        mov     eax,        ebx
+        sub     eax,        edi
+        
+        ; Checks if the pointer is already aligned
+        cmp     eax,        0
+        je      .aligned
+        
+        ; If not, computes the number of bytes to be written until we are
+        ; aligned on the next 16-byte boundary
+        mov     ecx,        16
+        sub     ecx,        eax
+        
+    .notaligned:
+        
+        ; Restores the original pointer in EDI
+        mov     edi,        ebx
+        
+        ; Stores the character to write in EAX, so we can access it as a byte
+        ; using AL
+        mov     eax,        esi
+        
+    .notaligned_loop:
+        
+        ; Checks if we have bytes to write
+        cmp     edx,        0
+        je      .end
+        
+        ; Writes a byte into the memory buffer
+        mov     [ edi ],    al
+        
+        ; Advances the memory pointer
+        inc     edi
+        
+        ; Decreases the number of bytes to write (EDX is the total, ECX is the
+        ; number of bytes to write until we're aligned to a 16-byte boundary)
+        dec     ecx
+        dec     edx
+        
+        ; Checks if we are aligned to a 16-byte boundary. If so, the SSE
+        ; instructions can be safely used
+        cmp     ecx,        0
+        je      .aligned
+        jmp     .notaligned_loop
+        
+    .aligned:
+        
+        ; Writes 128 bytes at a time, if possible
+        cmp     edx,        128
+        jge     .aligned_128
+        
+        ; Writes 64 bytes at a time, if possible
+        cmp     edx,        64
+        jge     .aligned_64
+        
+        ; Writes 32 bytes at a time, if possible
+        cmp     edx,        32
+        jge     .aligned_32
+    
+        ; Writes 16 bytes at a time, if possible
+        cmp     edx,        16
+        jge     .aligned_16
+        
+    .aligned_end:
+        
+        ; We're aligned on a 16-byte boundary, but there's not enough bytes
+        ; to write in order to use the SSE instructions, so writes the
+        ; remaining bytes one by one
+        mov     ecx,            edx
+        mov     eax,            esi
+        jmp     .notaligned_loop
+        
+    .aligned_128:
+        
+        ; Writes 128 bytes into the memory buffer
+        movdqa  [ edi       ],  xmm0
+        movdqa  [ edi +  16 ],  xmm0
+        movdqa  [ edi +  32 ],  xmm0
+        movdqa  [ edi +  48 ],  xmm0
+        movdqa  [ edi +  64 ],  xmm0
+        movdqa  [ edi +  80 ],  xmm0
+        movdqa  [ edi +  96 ],  xmm0
+        movdqa  [ edi + 112 ],  xmm0
+        
+        ; Advances the memory pointer and decreases the number of bytes to write
+        add     edi,            128
+        sub     edx,            128
+        
+        ; Next bytes - Writes 128 bytes at a time, if possible
+        cmp     edx,            128
+        jge     .aligned_128
+        
+        ; Next bytes - Writes 64 bytes at a time, if possible
+        cmp     edx,            64
+        jge     .aligned_64
+        
+        ; Next bytes - Writes 32 bytes at a time, if possible
+        cmp     edx,            32
+        jge     .aligned_32
+        
+        ; Next bytes - Writes 16 bytes at a time, if possible
+        cmp     edx,            16
+        jge     .aligned_16
+        
+        ; Checks if we're done writing bytes
+        cmp     edx,            0
+        je      .end
+        
+        ; If we still have byte to write, writes them one by one
+        jmp     .aligned_end
+        
+    .aligned_64:
+        
+        ; Writes 64 bytes into the memory buffer
+        movdqa  [ edi      ],   xmm0
+        movdqa  [ edi + 16 ],   xmm0
+        movdqa  [ edi + 32 ],   xmm0
+        movdqa  [ edi + 48 ],   xmm0
+        
+        ; Advances the memory pointer and decreases the number of bytes to write
+        add     edi,            64
+        sub     edx,            64
+        
+        ; Next bytes - Writes 64 bytes at a time, if possible
+        cmp     edx,            64
+        jge     .aligned_64
+        
+        ; Next bytes - Writes 32 bytes at a time, if possible
+        cmp     edx,            32
+        jge     .aligned_32
+        
+        ; Next bytes - Writes 16 bytes at a time, if possible
+        cmp     edx,            16
+        jge     .aligned_16
+        
+        ; Checks if we're done writing bytes
+        cmp     edx,            0
+        je      .end
+        
+        ; If we still have byte to write, writes them one by one
+        jmp     .aligned_end
+        
+    .aligned_32:
+        
+        ; Writes 32 bytes into the memory buffer
+        movdqa  [ edi      ],   xmm0
+        movdqa  [ edi + 16 ],   xmm0
+        
+        ; Advances the memory pointer and decreases the number of bytes to write
+        add     edi,            32
+        sub     edx,            32
+        
+        ; Next bytes - Writes 32 bytes at a time, if possible
+        cmp     edx,            32
+        jge     .aligned_32
+        
+        ; Next bytes - Writes 16 bytes at a time, if possible
+        cmp     edx,            16
+        jge     .aligned_16
+        
+        ; Checks if we're done writing bytes
+        cmp     edx,            0
+        je      .end
+        
+        ; If we still have byte to write, writes them one by one
+        jmp     .aligned_end
+        
+    .aligned_16:
+        
+        ; Writes 16 bytes into the memory buffer
+        movdqa  [ edi ],        xmm0
+        
+        ; Advances the memory pointer and decreases the number of bytes to write
+        add     edi,            16
+        sub     edx,            16
+        
+        ; Next bytes - Writes 16 bytes at a time, if possible
+        cmp     edx,            16
+        jge     .aligned
+        
+        ; Checks if we're done writing bytes
+        cmp     edx,            0
+        je      .end
+        
+        ; If we still have byte to write, writes them one by one
+        jmp     .aligned_end
+    
+    .end:
+        
+        ; Return value - Gets the original pointer
+        mov     eax,            ebx
+        
+        ; Restores saved registers
+        pop     ebx
+        pop     esi
+        pop     edi
+        pop     ebp
+        
+        ret
 
 ;-------------------------------------------------------------------------------
 ; 32-bits optimized memset() function
